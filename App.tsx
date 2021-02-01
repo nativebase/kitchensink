1904b70b1fea24a527edb1a2b8053f650ee56953
ashishiiit01 <ashishiiit01@gmail.com>
2021-02-01 17:50:30 +0530

    Added intl package

--- App.tsx
+++ App.tsx
@@ -1,8 +1,12 @@
-import React from "react";
-import { NativeBaseProvider, Box, Flex } from "native-base";
-import { RootStack } from "./src/navigators/RootStack";
-import { NavigationContainer } from "@react-navigation/native";
-import { kitchensinkTheme } from "./src/theme";
+import 'intl';
+import 'intl/locale-data/jsonp/en';
+
+import React from 'react';
+import { NativeBaseProvider, Box, Flex } from 'native-base';
+import { RootStack } from './src/navigators/RootStack';
+import { NavigationContainer } from '@react-navigation/native';
+import { kitchensinkTheme } from './src/theme';
+
 export default function App() {
   return (
     <NativeBaseProvider theme={kitchensinkTheme}>
